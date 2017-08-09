879da026ae3979a5e87f8f6e79c8a4b7fa5f7354
Chad Clarke <cclarke@napoleonproducts.com>
2017-08-09 08:41:11 -0400

    updated styling

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Chad Clarke' });
 });
 
+/* GET portfolio page. */
+router.get('/', function(req, res, next) {
+  res.render('portfolio', { title: 'My Works' });
+});
+
 module.exports = router;
